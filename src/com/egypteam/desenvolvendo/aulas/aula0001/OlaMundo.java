26b05287662b3cf91264bbb5b6ad4f3fda739427
Pedro Ferreira <pedrofmj@gmail.com>
2022-04-30 16:20:51 -0300

    Mais comentários

--- src/com/egypteam/desenvolvendo/aulas/aula0001/OlaMundo.java
+++ src/com/egypteam/desenvolvendo/aulas/aula0001/OlaMundo.java
@@ -1,10 +1,15 @@
 package com.egypteam.desenvolvendo.aulas.aula0001;
 
 public class OlaMundo {
+
     public static void main(String[] args) {
 
         // Isto é um comentário
+        // Comandos simples basicamente chamam métodos de um objeto, que é uma instância de uma classe
+        // O comando abaixo chama o método "println" do objeto instanciado em System.out, com um único parâmetro "Olá, Mundo"
+        // Este método desempenha a função de imprimir um texto na saída padrão, que no caso é a tela da linha de comando.
         System.out.println("Olá, Mundo!");
 
     }
+
 }
