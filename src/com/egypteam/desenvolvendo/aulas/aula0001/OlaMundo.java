873da3a3ce7ed794b99ed5c450376dec43302030
Pedro Ferreira <pedrofmj@gmail.com>
2022-04-30 16:18:45 -0300

    Comentário

--- src/com/egypteam/desenvolvendo/aulas/aula0001/OlaMundo.java
+++ src/com/egypteam/desenvolvendo/aulas/aula0001/OlaMundo.java
@@ -2,6 +2,9 @@ package com.egypteam.desenvolvendo.aulas.aula0001;
 
 public class OlaMundo {
     public static void main(String[] args) {
+
+        // Isto é um comentário
         System.out.println("Olá, Mundo!");
+
     }
 }
