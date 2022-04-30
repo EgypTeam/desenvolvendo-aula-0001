cc98ac760841d1a3948a34a523a1dc2d80ddfd92
Pedro Ferreira <pedrofmj@gmail.com>
2022-04-30 16:24:30 -0300

    Mais comentários

--- src/com/egypteam/desenvolvendo/aulas/aula0001/OlaMundo.java
+++ src/com/egypteam/desenvolvendo/aulas/aula0001/OlaMundo.java
@@ -1,7 +1,17 @@
 package com.egypteam.desenvolvendo.aulas.aula0001;
 
+/*
+ * Esta é a classe OlaMundo
+ * Estamos descrevendo este comentário em outro formato que é o comentário multi-linha (ou multiline - em programação, costumamos manter alguns termos em inglês)
+ * Essa classe possui um método estático (conversaremos sobre isso mais para frente), chamado "main".
+ */
 public class OlaMundo {
 
+    /*
+     * O método estático main é um padrão adotado pelo Java e pela JVM
+     * Quando você executa a classe através do comando java, a JVM faz uma chamada para o método main, que é chamado de entry point (ou ponto de entrada)
+     * Por essa razão, quando você compilou o programa e executou a classe, o programa imprimiu (exibiu) na tela o texto "Olá, Mundo".
+     */
     public static void main(String[] args) {
 
         // Isto é um comentário
