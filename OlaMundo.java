88546f2d961c0bf5eda03322edb57f69ed0669d1
Pedro Ferreira <pedrofmj@gmail.com>
2022-12-06 06:13:30 -0300

    TIA CURSO INTRODUCAO A JAVA - MODULO 1 - CAPITULO 1 - AULA 1

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class OlaMundo {
     public static void main(String[] args) {
-        System.out.println("Olá, Mundo!");
+
+        int sum = 0;
+
+        if (args.length > 0) {
+
+            for (int i = 0; i < args.length; i++) {
+                sum = sum + Integer.parseInt(args[i]);
+            }
+    
+            System.out.println(String.format("O valor da soma eh %d", sum));
+
+        } else {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Entre com o proximo numero: ");
+            while (scanner.hasNextInt()) {
+                int a = scanner.nextInt();
+                sum = sum + a;
+                System.out.print("Entre com o proximo numero: ");
+            }
+
+            System.out.println();
+            System.out.println("-----------------------");
+            System.out.println(String.format("O valor da soma eh %d", sum));
+
+        }
+
     }
 }
